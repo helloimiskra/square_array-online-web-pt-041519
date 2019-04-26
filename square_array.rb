88c6be3d79b580a5de3d4e9275be08bbc2788e64
Iskra B <ibatistapoblete@gmail.com>
2019-04-26 03:22:50 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
-def square_array(array)
-  new_array = []
-  array.each do |x|
-    new_array << x ** 2
-  end
-  return new_array# your code here
-end
+#def square_array(array)
+  #new_array = []
+  #array.each do |x|
+  #  new_array << x ** 2
+  #end
+  #return new_array# your code here
+#end
 
 
 def square_array(array)
